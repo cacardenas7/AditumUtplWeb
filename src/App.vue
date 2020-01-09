9d7c9040aa3a665dfc9c1e5d24cf43d4a06940d4
jtaramon <jarz_1998@yahoo.es>
2020-01-08 22:06:45 -0500

    Final

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@
       <router-link :to="{name:'registro'}" v-if="!existingUser">Registro</router-link>
       <router-link :to="{name:'inicio'}" v-if="existingUser">Inicio</router-link>
       <router-link :to="{name:'ingreso'}" v-if="!existingUser">Ingreso</router-link>
-    {{existingUser}}
-      <a @click="logoutUsers" v-if="existingUser">Cerrar Sesion</a>
-      <a @click="logoutUsers" v-if="existingUser">Cerrar Sesion</a>
       <a @click="logoutUsers" v-if="existingUser">Cerrar Sesion</a>
     </div>
     <router-view/>
